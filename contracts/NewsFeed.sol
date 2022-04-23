4db076f94597a6ce7f6541f7eabc6f682abc69c3
diptarag1 <diptarag1@gmail.com>
2022-04-23 10:00:33 +0530

    contract refactor

--- contracts/NewsFeed.sol
+++ contracts/NewsFeed.sol
@@ -5,35 +5,20 @@ contract NewsFeed
 {
     uint256 constant reportThreshold=5; //if no. of reports reaches this, validtors will be assigned
     uint256 constant validatorCount=3; //no. of validators assigned to each news article for validation
+    uint256 constant publishingCost=2; //cost to publish an article on the app
 
     uint256 public newsCount=0;
     enum State { Unverified, Fake, Verified }
-    enum Role { Reader, Validator, Publisher }
     mapping(uint256=>News) public news_feed;
-    mapping(address => mapping(uint => bool)) accountRoles;
     address public creator;
     mapping(address => News) assignedArticle; //stores mapping of what articles is assigned to each validator at a given moment
-    address[] freeValidators; //list of validators who have are idle
+    address[] freeValidators; //list of validators who are idle
     address[] articleStake;
     mapping(address => mapping(uint => uint)) readerStake; //stores amount reader stakes on an article
+    Accounts accContract;
 
-    function accountAddRole(address account, Role role) internal { //adds specified role to account
-        require(
-            msg.sender == creator
-        );
-        accountRoles[account][uint(role)] = true;
 
-        if(role == Role.Validator) {
-            freeValidators.push(account);
-        }
-    }
-
-    function accountHasRole(address account, Role role) internal
-    view returns (bool) { //checks whether account has specified role
-        return accountRoles[account][uint(role)];
-    }
-
-    struct News{
+    struct News {
         uint256 id;
         address publisher;
         string title;
@@ -43,16 +28,19 @@ contract NewsFeed
         address[]  upvotes_address;
         uint256 downvotes;
         address[] downvotes_address;
-        uint256 reportCount;
     }
 
-    constructor() public { //called when contract is first created
+    constructor(Accounts _accContract) public { //called when contract is first created
         creator = msg.sender;
+        accContract = _accContract;
     }
 
     function addNews (string memory _title, string memory _description) payable public { //adds a news article
         require(
-            accountHasRole(msg.sender, Role.Publisher)
+            accContract.accountHasRole(msg.sender, uint(Accounts.Role.Publisher))
+        );
+        require(
+            msg.value == publishingCost
         );
         newsCount++;
         news_feed[newsCount]=News({
@@ -64,10 +52,11 @@ contract NewsFeed
             upvotes: 0,
             downvotes: 0,
             upvotes_address: new address [](0),
-            downvotes_address: new address [](0),
-            reportCount: 0
+            downvotes_address: new address [](0)
         });
         payable(creator).transfer(msg.value);
+
+        assignValidators(newsCount);
     }
 
     function decideState(uint256 _index) private { //decides the state once all validators have voted
@@ -79,20 +68,6 @@ contract NewsFeed
         }
     }
 
-
-    function report(uint256 _index) payable public { //reader reports an article as fake news
-        require(
-            accountHasRole(msg.sender, Role.Reader)
-        );
-        if(news_feed[_index].news_state != State.Unverified) return;
-
-        news_feed[_index].reportCount++;
-        if(news_feed[_index].reportCount >= reportThreshold) {
-            assignValidators(_index);
-        }
-        readerStake[msg.sender][_index] = msg.value;
-        payable(creator).transfer(msg.value);
-    }
 
     function assignValidators(uint256 _index) private { //contract assigns validators when enough readers have reported
         uint cnt = validatorCount;
@@ -107,7 +82,7 @@ contract NewsFeed
 
     function addUpvote(uint256 _index) private { //validator votes as legitmiate news
         require(
-            accountHasRole(msg.sender, Role.Validator)
+            accContract.accountHasRole(msg.sender, uint(Accounts.Role.Validator))
         );
         news_feed[_index].upvotes_address.push(msg.sender);
         news_feed[_index].upvotes++;
@@ -115,9 +90,32 @@ contract NewsFeed
 
     function addDownvote(uint256 _index) private { //validator votes as fake news
         require(
-            accountHasRole(msg.sender, Role.Validator)
+            accContract.accountHasRole(msg.sender, uint(Accounts.Role.Validator))
         );
         news_feed[_index].downvotes_address.push(msg.sender);
         news_feed[_index].downvotes++;
+    }
+}
+
+
+contract Accounts {
+    address public creator;
+    mapping(address => mapping(uint => bool)) accountRoles;
+    enum Role { Reader, Validator, Publisher }
+    address[] freeValidators;
+
+    function accountAddRole(address account, uint role) internal { //adds specified role to account
+        require(
+            msg.sender == creator
+        );
+        accountRoles[account][role] = true;
+
+        if(role == uint(Role.Validator)) {
+            freeValidators.push(account);
+        }
+    }
+
+    function accountHasRole(address account, uint role) public returns (bool) { //checks whether account has specified role
+        return accountRoles[account][role];
     }
 }
